aa75325f8b7563a6b2b0416d0026ed7fbbb2ced6
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-06-19 06:37:23 +0530

    Time: 34 ms (6.44%), Space: 58.5 MB (17.52%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private int minLimit(int[] bloomDay)
+    {
+        int min = bloomDay[0];
+        for(int day:bloomDay)
+        {
+            min = Math.min(min,day);
+        }
+           return min;
+    }
+    private int maxLimit(int[] bloomDay)
+    {
+        int max = bloomDay[0];
+        for(int day:bloomDay)
+        {
+            max = Math.max(max,day);
+        }
+        return max;
+    }
+    private boolean isPossible(int[] bloomDay, int day, int m, int k)
+    {
+        int count = 0, noOfBoq = 0;
+        for(int i:bloomDay)
+        {
+            if(i<=day) count++;
+            else
+            {
+                noOfBoq += count/k;
+                count = 0;
+            }
+        }
+        noOfBoq += count/k;
+        if(noOfBoq>=m) return true;
+        else return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        if(m*k>bloomDay.length) return -1;
+        int low = minLimit(bloomDay);
+        int high = maxLimit(bloomDay);
+        int ans = -1;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(isPossible(bloomDay,mid,m,k))
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+}
