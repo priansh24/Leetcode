09c33054883ceadd0fe268540bd9af675af62fcc
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-06-23 05:21:17 +0530

    Time: 7 ms (96.6%), Space: 52.9 MB (94.83%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,47 +1,47 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        // int n = nums.length;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]%2==0) nums[i] = 0;
-        //     else nums[i] = 1;
-        //     System.out.print(nums[i]+" ");
-        // }
-        // // for(int i=0;i<nums.length;i++)
-        // // {
-        // //     nums[i] = sum+nums[i];
-        // //     sum = nums[i];
-        // // }
-        // // sum = 0;
-        // int sum = 0;
-        // int ans = 0;
-        // int i=0,j = 0;
-        // while(j<n)
-        // {       
-        //     sum+=nums[j];       
-        //     while(sum>k)
-        //     {
-        //         sum -= nums[i];
-        //         i++;
-        //     }  
-        //     ans += j-i+1;
-        //     j++;
-        // }
-        
-        // System.out.println("\nANS: "+ans);
-        // return ans;
+    private int helper(int nums[],int k)
+    {
         int n = nums.length;
-        int[] cnt = new int[n + 1];
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
+        int ans=0;
+        for(int num:nums)
+        {
+            if(k<0) return 0;
+            ans = 0;
+            int sum = 0;
+            int i=0,j = 0;
+            while(j<n)
+            {       
+                sum+=nums[j]%2;       
+                while(sum>k)
+                {
+                    sum -= nums[i]%2;
+                    i++;
+                }  
+                ans += j-i+1;
+                j++;
             }
-            cnt[t]++;
         }
+            return ans;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0;
+        int ans = 0;
+        int[] map = new int[n+1];
+        map[0]=1;
+        int count = 0;
+        for(int i = 0;i<n;i++)
+        {
+            if(nums[i]%2==1) count++;
+            int diff  = count-k;
+            if(diff>=0)
+            {
+                ans += map[diff];
+            }
+            map[count]++;
+        }    
+        // int ans = helper(nums,k)-helper(nums,k-1);
+        // System.out.println("\nANS: "+ans);
         return ans;
-
     }
 }
