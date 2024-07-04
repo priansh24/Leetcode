518c66c2d0891ef4ad675900a891e2f53c76e231
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-04 15:56:20 +0530

    Time: 0 ms (100%), Space: 45.2 MB (66.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -10,7 +10,7 @@ class Solution {
 
     private int helper(int[][] grid, int[][] dp,int row, int col)
     {
-        if(col==0 && row==0) return grid[row][col];
+        if(col==0 && row==0) return grid[0][0];
         if(row<0 || col<0) return Integer.MAX_VALUE;
         if(dp[row][col]!=0) return dp[row][col]; 
         dp[row][col] = grid[row][col] + Math.min(helper(grid,dp,row-1,col), helper(grid,dp,row,col-1)); 
