18776c13815f2451221dbb7eb2c705ca9ca862a6
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-13 03:47:13 +0530

    Time: 0 ms (100%), Space: 40.1 MB (79.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,26 +4,25 @@ class Solution {
 
         int[][] dp = new int[m][n];
         // for(int[] row:dp) Arrays.fill(row,0);
-        // dp[0][0] = 1;
-        dp[m-1][n-1]=1;
+        dp[0][0] = 1;
         int up = 0, left = 0;      
-        for(int i = m-1;i>=0;i--)
+        for(int i = 0;i<m;i++)
         {
-            for(int j=n-1;j>=0;j--)
+            for(int j=0;j<n;j++)
             {
-                if(i==m-1 && j==n-1) dp[i][j] = 1;
+                if(i==0 && j==0) dp[i][j] = 1;
                 else
                 {
-                    if(i<m-1) up = dp[i+1][j];
+                    if(i>0) up = dp[i-1][j];
                     else up = 0;
 
-                    if(j<n-1) left  = dp[i][j+1];
+                    if(j>0) left  = dp[i][j-1];
                     else left = 0;
 
                     dp[i][j] = left+up;
                 }
             }
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
 }
