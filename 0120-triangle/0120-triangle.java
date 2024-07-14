f20f915a166fc7b74bb853873cda91e758f86dc3
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-14 20:11:20 +0530

    Time: 3 ms (64.91%), Space: 43.2 MB (92.48%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,34 +1,33 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int height = triangle.size();
-
-        // int[] dp = new int[height];
-        // int ind = 0;
-        // for(int i = 0;i<triangle.get(height-1).size();i++)
-        //     dp[ind++] = triangle.get(height-1).get(i);
+        int[] dp = new int[height];
+        int ind = 0;
+        for(int i:triangle.get(height-1))
+            dp[ind++] = i;
         
-        // for(int level = height-2;level>=0;level++)
-        // {
-        //     for(ind=0;ind<=level;ind++)
-        //     {
-        //         int currVal = triangle.get(level).get(ind);
-        //         int sum = Math.min(dp[ind],dp[ind+1]);
-        //         dp[ind] = currVal+sum;
-        //     }
-        // }
-        // return dp[0];
-
-        int[][] dp = new int[height+1][height+1];
-
-        for(int level = height-1;level>=0;level--)
+        for(int level = height-2;level>=0;level--)
         {
-            for(int ind = 0;ind<=level;ind++)
+            for(ind=0;ind<=level;ind++)
             {
                 int currVal = triangle.get(level).get(ind);
-                int sum = Math.min(dp[level+1][ind],dp[level+1][ind+1]);
-                dp[level][ind] = currVal+sum;
+                int sum = Math.min(dp[ind],dp[ind+1]);
+                dp[ind] = currVal+sum;
             }
         }
-        return dp[0][0];
+        return dp[0];
+
+        // int[][] dp = new int[height+1][height+1];
+
+        // for(int level = height-1;level>=0;level--)
+        // {
+        //     for(int ind = 0;ind<=level;ind++)
+        //     {
+        //         int currVal = triangle.get(level).get(ind);
+        //         int sum = Math.min(dp[level+1][ind],dp[level+1][ind+1]);
+        //         dp[level][ind] = currVal+sum;
+        //     }
+        // }
+        // return dp[0][0];
     }
 }
