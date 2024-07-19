f55c518213eeb9eb6672f75d01f8979e56af0d20
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-20 04:01:56 +0530

    Time: 3 ms (30.29%), Space: 42.4 MB (20.97%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.java
@@ -8,9 +8,19 @@ class Solution {
             {
                 String s = number.substring(0,i)+number.substring(i+1,number.length());
                 if(max.length()==0) max = s;
-                else max = s.compareTo(max)>0?s:max;
+                // else max = s.compareTo(max)>0?s:max;
+                else max = findGreaterString(s,max);
             }
         }
         return max;
+    }
+    private String findGreaterString(String a, String b)
+    {
+        for(int i = 0;i<a.length();i++)
+        {
+            if(a.charAt(i)>b.charAt(i)) return a;
+            if(a.charAt(i)<b.charAt(i)) return b;
+        }
+        return a;
     }
 }
