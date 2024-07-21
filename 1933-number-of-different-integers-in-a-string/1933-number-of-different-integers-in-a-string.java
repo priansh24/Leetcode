9d557df09fe8fced1c92d2e9adf93f7327370bf9
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-22 04:52:34 +0530

    Time: 2 ms (99.4%), Space: 41.9 MB (63.96%) - LeetHub

---
+++ 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numDifferentIntegers(String word) {
+int l = word.length();
+        if (l == 1) {
+            return word.charAt(0) <= '9' ? 1 : 0;
+        }
+        
+        // -- Solution 1:
+        char[] arr = word.toCharArray();
+     
+        if ((arr[0] == '0' && arr[1] <= '9') || arr[0] > '9') {
+            arr[0] = ' ';
+        }
+
+        for (int i = 1; i < l; i++) {
+            if (i < l - 1 && arr[i] == '0' && arr[i - 1] == ' ' && arr[i + 1] <= '9') {
+                arr[i] = ' ';
+            } else if (arr[i] > '9') {
+                arr[i] = ' ';
+            }
+        }
+        HashSet<String> set = new HashSet<>();
+        StringBuilder num = new StringBuilder();
+        for (char c : arr) {
+            if (c != ' ') {
+                num.append(c);
+            } else if (num.length() > 0) {
+                set.add(num.toString());
+                num.setLength(0);
+            }
+        }
+        if (num.length() > 0) {
+            set.add(num.toString());
+        }
+        return set.size();
+    }
+}
