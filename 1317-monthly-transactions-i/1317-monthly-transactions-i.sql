576843429380877f886cc223d226ac65cc328a20
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-25 03:45:32 +0530

    Time: 756 ms (28.8%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -3,6 +3,6 @@ select substr(trans_date,1,7) as month,country,
 count(id) as trans_count,
 sum(if(state='approved',1,0)) as approved_count,
 sum(amount) as trans_total_amount,
- sum(if(state='approved',amount,0)) as approved_total_amount
- from transactions
- group by month,country;
+sum(if(state='approved',amount,0)) as approved_total_amount
+from transactions
+group by month,country;
