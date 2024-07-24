4bdbd2d79548b36b51a807c336bb3e310b6c72c0
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-07-25 04:06:06 +0530

    Time: 532 ms (73.69%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -3,4 +3,5 @@ select round(count(distinct player_id)/(select count(distinct player_id) from ac
 from activity
 where (player_id,Date_SUB(event_date,interval 1 Day))
 in
-(Select player_id,min(event_date) as first_login from activity group by player_id);
+(Select player_id,min(event_date) as first_login from activity group by player_id)
+;
