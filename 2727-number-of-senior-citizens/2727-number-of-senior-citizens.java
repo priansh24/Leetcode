1026f39e5746f20c55c5d6c3f119b47971a087f9
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-08-02 01:05:25 +0530

    Time: 1 ms (82.64%), Space: 42.8 MB (27.08%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -1,11 +1,11 @@
 class Solution {
     public int countSeniors(String[] details) {
-        List<Integer> list = new ArrayList<>();
+        int count = 0;
         for(String p:details)
         {
             int age = Integer.parseInt(p.substring(11,13));
-            if(age>60) list.add(age);
+            if(age>60) count++;
         }
-        return list.size();
+        return count;
     }
 }
