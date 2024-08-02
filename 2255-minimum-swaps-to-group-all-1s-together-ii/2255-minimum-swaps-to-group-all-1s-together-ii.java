bc8f5f88cd6fde5fcc2296d1d8f400223a675541
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-08-02 06:52:59 +0530

    Time: 10 ms (46.7%), Space: 61.4 MB (20.32%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minSwaps(int[] arr) {
         int n = arr.length;
-        // int[] nums = new int[2*n];
-        // int ind = 0;
-        // for(int i:arr) nums[ind++] = i;
-        // for(int i:arr) nums[ind++] = i;
+        int[] nums = new int[2*n];
+        int ind = 0;
+        for(int i:arr) nums[ind++] = i;
+        for(int i:arr) nums[ind++] = i;
 
         int totalOnes = 0;
         for(int i:arr) if(i==1) totalOnes++;
@@ -16,8 +16,8 @@ class Solution {
         int start = 0;
         for(int end = totalOnes;end<2*n;start++,end++)
         {
-            if(arr[start%n]==0) swap--; // arr[start%n] is same as nums[start]
-            if(arr[end%n]==0) swap++;   // arr[end%n] is same as nums[end]
+            if(nums[start]==0) swap--; // arr[start%n] is same as nums[start]
+            if(nums[end]==0) swap++;   // arr[end%n] is same as nums[end]
             ans = Math.min(ans,swap);
         }
         return ans;
