ca0f8e481057697b0233fd85a28bc6c3e4e5a14c
Priyansh Singhal <86052202+priansh24@users.noreply.github.com>
2024-08-02 06:51:56 +0530

    Time: 10 ms (46.7%), Space: 61.5 MB (16.09%) - LeetHub

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSwaps(int[] arr) {
+        int n = arr.length;
+        int[] nums = new int[2*n];
+        int ind = 0;
+        for(int i:arr) nums[ind++] = i;
+        for(int i:arr) nums[ind++] = i;
+
+        int totalOnes = 0;
+        for(int i:arr) if(i==1) totalOnes++;
+
+        int swap = 0;
+        for(int start = 0;start<totalOnes;start++) if(arr[start]==0) swap++;
+        
+        int ans = swap;
+        int start = 0;
+        for(int end = totalOnes;end<2*n;start++,end++)
+        {
+            if(nums[start]==0) swap--; // arr[start%n] is same as nums[start]
+            if(nums[end]==0) swap++;   // arr[end%n] is same as nums[end]
+            ans = Math.min(ans,swap);
+        }
+        return ans;
+    }
+}
